e9a89105c29b075468aeae7395d9408fb5b59668
Mahamudul-Nobi-Mohon <mn.mohon@gmail.com>
2017-05-04 15:11:26 +0600

    c# desktop application data first

---
+++ EntityFrameworkCrud_02_05_2017/Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace EntityFrameworkCrud_02_05_2017
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxEmail = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.ID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.name = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.email = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.textBoxId = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.buttonUpdate = new System.Windows.Forms.Button();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(53, 63);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name";
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Location = new System.Drawing.Point(125, 63);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(100, 20);
+            this.textBoxName.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(53, 102);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Email";
+            // 
+            // textBoxEmail
+            // 
+            this.textBoxEmail.Location = new System.Drawing.Point(125, 102);
+            this.textBoxEmail.Name = "textBoxEmail";
+            this.textBoxEmail.Size = new System.Drawing.Size(100, 20);
+            this.textBoxEmail.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(266, 27);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Save";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ID,
+            this.name,
+            this.email});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 158);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(355, 254);
+            this.dataGridView1.TabIndex = 3;
+            this.dataGridView1.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellDoubleClick);
+            // 
+            // ID
+            // 
+            this.ID.HeaderText = "ID";
+            this.ID.Name = "ID";
+            // 
+            // name
+            // 
+            this.name.HeaderText = "Name";
+            this.name.Name = "name";
+            // 
+            // email
+            // 
+            this.email.HeaderText = "Email";
+            this.email.Name = "email";
+            // 
+            // textBoxId
+            // 
+            this.textBoxId.Location = new System.Drawing.Point(125, 27);
+            this.textBoxId.Name = "textBoxId";
+            this.textBoxId.ReadOnly = true;
+            this.textBoxId.Size = new System.Drawing.Size(100, 20);
+            this.textBoxId.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(62, 34);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(18, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "ID";
+            // 
+            // buttonUpdate
+            // 
+            this.buttonUpdate.Location = new System.Drawing.Point(266, 63);
+            this.buttonUpdate.Name = "buttonUpdate";
+            this.buttonUpdate.Size = new System.Drawing.Size(75, 23);
+            this.buttonUpdate.TabIndex = 2;
+            this.buttonUpdate.Text = "Update";
+            this.buttonUpdate.UseVisualStyleBackColor = true;
+            this.buttonUpdate.Click += new System.EventHandler(this.buttonUpdate_Click);
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.Location = new System.Drawing.Point(266, 99);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(75, 23);
+            this.buttonDelete.TabIndex = 2;
+            this.buttonDelete.Text = "Delete";
+            this.buttonDelete.UseVisualStyleBackColor = true;
+            this.buttonDelete.Click += new System.EventHandler(this.buttonDelete_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(377, 432);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.buttonDelete);
+            this.Controls.Add(this.buttonUpdate);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBoxEmail);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxId);
+            this.Controls.Add(this.textBoxName);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Entity framework crud";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxEmail;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn name;
+        private System.Windows.Forms.DataGridViewTextBoxColumn email;
+        private System.Windows.Forms.TextBox textBoxId;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button buttonUpdate;
+        private System.Windows.Forms.Button buttonDelete;
+    }
+}
+
---
+++ EntityFrameworkCrud_02_05_2017/Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EntityFrameworkCrud_02_05_2017
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            showAllData();
+        }
+
+        public void showAllData()
+        {
+            using (PersonCrudEntities db = new PersonCrudEntities())
+            {
+                List<person> v = db.people.ToList();
+                foreach (person item in v)
+                {
+                    int n = dataGridView1.Rows.Add();
+                    dataGridView1.Rows[n].Cells[0].Value = item.ID;
+                    dataGridView1.Rows[n].Cells[1].Value = item.name;
+                    dataGridView1.Rows[n].Cells[2].Value = item.email;
+                }
+
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Rows.Clear();
+            using (PersonCrudEntities db = new PersonCrudEntities())
+            {
+               person isExests = db.people.FirstOrDefault(r => r.email == textBoxEmail.Text);
+                if (isExests == null)
+                {
+                    person aPerson = new person
+                    {
+                        name = textBoxName.Text,
+                        email = textBoxEmail.Text
+                    };
+
+                    //is email exists or not
+
+                    db.people.Add(aPerson);
+                    db.SaveChanges();
+                    MessageBox.Show("data saved !");
+
+                }
+                else
+                {
+                    MessageBox.Show("email already exists.");
+                }
+
+                showAllData();
+            }
+        }
+
+        private void dataGridView1_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+            textBoxId.Text = dataGridView1.CurrentRow.Cells[0].Value.ToString();
+            textBoxName.Text = dataGridView1.CurrentRow.Cells[1].Value.ToString();
+            textBoxEmail.Text = dataGridView1.CurrentRow.Cells[2].Value.ToString();
+        }
+
+        private void buttonUpdate_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Rows.Clear();
+
+            if (textBoxId.Text != "")
+            {
+                using (PersonCrudEntities db = new PersonCrudEntities())
+                {
+                    int id = Convert.ToInt32(textBoxId.Text);
+                    person isExests = db.people.FirstOrDefault(r => r.email == textBoxEmail.Text);
+                    person aPerson = db.people.FirstOrDefault(r => r.ID == id);
+                    if (isExests == null)
+                    {
+
+                        aPerson.name = textBoxName.Text;
+                        aPerson.email = textBoxEmail.Text;
+
+
+                        //is email exists or not
+
+                        //db.people.Add(aPerson);
+                        db.SaveChanges();
+                        MessageBox.Show("Updated Successfully !");
+
+                    }
+                    else
+                    {
+                        MessageBox.Show("email already exists.");
+                    }
+
+                }
+                }
+                else{
+                    MessageBox.Show("Please Select a member for update.");
+                }
+                showAllData();
+            }
+        
+
+        private void buttonDelete_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Rows.Clear();
+            if(textBoxId.Text != "")
+            {            
+            using (PersonCrudEntities db = new PersonCrudEntities())
+            {
+                int id = Convert.ToInt32(textBoxId.Text);
+                person aPerson = db.people.FirstOrDefault(r => r.ID == id);
+
+
+                     db.people.Remove(aPerson);
+                     db.SaveChanges();
+                    MessageBox.Show("Deleted successfully !");
+
+                
+
+            }
+            }
+            else
+            {
+                MessageBox.Show("Please select a Member for delete.");
+            }
+            showAllData();
+        }
+    }
+}
